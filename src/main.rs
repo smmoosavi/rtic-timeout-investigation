c215a59f53b3f36ee893c9bb782bcd45aa4c9ff0
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2024-02-20 17:32:00 +0330

    hello world!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+#![feature(type_alias_impl_trait)]
+
+use panic_rtt_target as _;
+
+#[rtic::app(device = stm32l0xx_hal::pac, peripherals = true, dispatchers = [SPI1, SPI2])]
+mod app {
+    use rtt_target::{rprintln, rtt_init_print};
+
+    #[shared]
+    struct Shared {}
+
+    #[local]
+    struct Local {}
+
+    #[init]
+    fn init(_ctx: init::Context) -> (Shared, Local) {
+        rtt_init_print!();
+
+        rprintln!("Hello, world!");
+
+        (Shared {}, Local {})
+    }
 }
